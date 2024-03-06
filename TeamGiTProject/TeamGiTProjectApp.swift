be0d452b1902360434ab699471d323f0e1bbfd09
Filip <fmilenkovic03@gmail.com>
2024-03-06 14:57:44 +0100

    Initial Commit

---
+++ TeamGiTProject/TeamGiTProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamGiTProjectApp.swift
+//  TeamGiTProject
+//
+//  Created by Apprenant98 on 06/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TeamGiTProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
