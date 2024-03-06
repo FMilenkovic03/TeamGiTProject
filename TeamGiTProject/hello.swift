eff4c9ba38d2c68499831ffd62a183c81cf5296f
Filip <fmilenkovic03@gmail.com>
2024-03-06 15:36:48 +0100

    Ajout du fichier MonFichier.swift sur la branche MilenkovicFilip

---
+++ TeamGiTProject/hello.swift
@@ -0,0 +1,18 @@
+//
+//  hello.swift
+//  TeamGiTProject
+//
+//  Created by Apprenant98 on 06/03/2024.
+//
+
+import SwiftUI
+
+struct hello: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    hello()
+}
